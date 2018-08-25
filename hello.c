c81c881dc14c4d32a4c98ca0cd11193b66b2ef8a
aor2018 <42587218+aor2018@users.noreply.github.com>
2018-08-24 19:41:29 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Artiom Dolghi
+ * Date: 2018/21/08
+ *
+ * A program that displays name and major
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Artiom Dolghi, Computer Science");
+
+  return 0;
+}
